aaef0795f186f09c3e1b69c6124cb2c69d68a605
juancg7165 <juancg7165@gmail.com>
2024-10-09 12:41:53 -0300

    v1.1

--- navbar.js
+++ navbar.js
@@ -1,8 +1,6 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('navbar.html')
-        .then(response => response.text())
-        .then(data => {
-            document.getElementById('navbar').innerHTML = data;
-        })
-        .catch(error => console.error('Error al cargar el navbar:', error));
-});
+fetch('navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
