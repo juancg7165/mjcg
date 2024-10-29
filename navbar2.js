b873ab66ad499ae30342ee2b06ddceb7d702fd1f
juancg7165 <juancg7165@gmail.com>
2024-10-29 15:17:40 -0300

    asg

---
+++ navbar2.js
@@ -0,0 +1,15 @@
+fetch('../navbar.html')
+            .then(response => response.text())
+            .then(data => {
+                document.getElementById('navbar').innerHTML = data;
+            })
+            .catch(error => console.error('Error loading navbar:', error));
+function toggleMenu() {
+        const menu = document.querySelector('.nav.first');
+        // Verificar si el menú está visible o no
+        if (menu.style.display === 'block') {
+            menu.style.display = 'none';  // Colapsar
+        } else {
+            menu.style.display = 'block'; // Expandir
+        }
+    }
